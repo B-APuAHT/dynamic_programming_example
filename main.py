d4b1bc183429a5b1bfca82b0bb178be59d3c943e
Alexander <sasha-kurlov@yandex.ru>
2013-05-28 00:24:26 +0600

    complete algorithm

--- main.py
+++ main.py
@@ -2,15 +2,14 @@ from tkinter import *
 #считываем данные из файла data.txt и заполняем двумерный массив table
 table = [[float(i) for i in line.split()] for line in open('data.txt')]
 
-depth = 5
-cash = len(table)               #находим кол-во денег, это просто высота таблицы
-amount = cash * max(table[-1])  #находим максимальную ставку для
-variants = []
+depth = 5                      #"глубина" перебора
+cash = len(table)              #находим кол-во денег, это просто высота таблицы               
+variants = [cash * max(table[-1])] #список вариантов размещения, изначально дбавляем произведение денег на максимальную ставку
+
+#начинаем перебирать возможные варианты в зависимости от глубины
 for k in range(depth):
     tmp = [ max(table[k])*(k+1) ]
     tmp += [ max(table[x])*(x+1) + max(table[k-x-1])*(k-x) for x in range(k//2) ]
-    variants += tmp
+    variants += [max(tmp) + (cash-k-1) * max(table[cash-k-2])]
+print(format(max(variants), '.2f'))  #печатаем максимальный вариант в формате с двумя цифрами после точки
 
-#favorite_index = table.index(max(tmp))
-#temp_amount = 
-#root = Tk()
